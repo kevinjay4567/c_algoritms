05274ae195b6777a3c8b2f15f8a7ef9362d3e1d1
Kevin Jay Herrera <kjayh@unal.edu.co>
2026-04-10 13:25:42 -0500

    feature: implementacion de eliminar item en array y mejoras en el agregar item al array

--- array.c
+++ array.c
@@ -10,9 +10,11 @@ typedef struct {
 } Array;
 
 Array create_list(size_t element_size) {
-  Array list = {1, element_size, calloc(element_size, element_size)};
-
-  return list;
+  return (Array) {
+    .length = 0,
+    .element_size = element_size,
+    .data = malloc(element_size)
+  };
 }
 
 void *get(Array list, u_int index) {
@@ -25,6 +27,12 @@ void *get(Array list, u_int index) {
 }
 
 void add(Array *list, void *value) {
+  if (list->length == 0) {
+    memcpy(list->data, value, list->element_size);
+    list->length += 1;
+    return;
+  }
+
 	size_t n_len = list->length + 1;
 
 	void *n_data = realloc(list->data, (list->length * list->element_size));
@@ -39,17 +47,35 @@ void add(Array *list, void *value) {
 
 void set(Array *list, u_int index, void *value) {
   if (index >= list->length) {
-    printf("Error: fuera de rango");
+    printf("Error: fuera de rango\n");
   }
   void *dest = list->data + (index * list->element_size);
   memcpy(dest, value, list->element_size);
 }
 
-void delete(Array *list) { free(list->data); }
+void delete(Array *list, u_int index) {
+  if (index >= list->length) {
+    printf("Error: fuera de rango\n");
+  }
+
+  list->length = list->length - 1;
+
+  for (int i = index; i < list->length; i++) {
+    void *dest = list->data + i;
+    void *value = list->data + i + 1;
+    memcpy(dest, value, list->element_size);
+  }
+
+  list->data = realloc(list->data, list->length);
+}
+
+void free_array(Array *list) {
+  free(list->data);
+}
+
 
 int main() {
   Array letters = create_list(sizeof(char));
-  int *p;
   char n = 'k';
   add(&letters, &n);
   n = 'e';
@@ -61,11 +87,13 @@ int main() {
   n = 'n';
   add(&letters, &n);
 
+  delete(&letters, 0);
+
   for (int i = 0; i < (int)letters.length; i++) {
     printf("%c\n", (*(char *)get(letters, i)));
   }
 
-  delete(&letters);
+  free_array(&letters);
 
   return 0;
 }
